a49d51d70b89b4a84de0088da7f2a4e2acef91a2
legendarycoconutkai <73057952+legendarycoconutkai@users.noreply.github.com>
2025-03-19 23:40:51 +0800

    added map api

--- lib/screens/map/map.dart
+++ lib/screens/map/map.dart
@@ -1,10 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
  
- class MapPage extends StatelessWidget {
-   @override
-   Widget build(BuildContext context) {
-     return Scaffold(
-       body: Center(child: Text("Map Page", style: TextStyle(fontSize: 20))),
-     );
-   }
- }
+class MapPage extends StatefulWidget{
+  const MapPage({super.key});
+
+  @override
+  State<MapPage> createState() => _MapPageState();
+}
+
+class _MapPageState extends State<MapPage>{
+  static const LatLng _pGooglePlex = LatLng(45.521563, -122.677433);
+
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      body: GoogleMap(initialCameraPosition: CameraPosition(target: _pGooglePlex, zoom: 15.0),),
+    );
+  }
+}
